3d07be199a78c6243b4b25e8b5ee1dc0aab25216
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-22 19:48:42 -0300

    implementação do try catch

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use http\Client\Curl\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use function Pest\Laravel\json;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -21,15 +22,24 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
 
-        $validatedData['password'] = bcrypt($validatedData['password']);
-        $user = User::create($validatedData);
-        return response()->json($user, 201);
+            $validatedData['password'] = bcrypt($validatedData['password']);
+            $user = User::create($validatedData);
+
+            DB::commit();
+
+            return response()->json($user, 201);
+
+        }catch (\Exception $exception){
+            DB::rollBack();
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
     }
 
     /**
@@ -42,7 +52,7 @@ class UserController extends Controller
         if (is_null($user)) {
             return response()->json('Usuário não encontrado.', 404);
         }
-        return response()->json($user);
+        return response()->json($user, 201);
     }
 
     /**
@@ -50,18 +60,23 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $user = User::find($id);
-        if (is_null($user)) {
-            return response()->json('Usuário não encontrado.', 404);
+        try {
+            $user = User::find($id);
+            if (is_null($user)) {
+                return response()->json('Usuário não encontrado.', 404);
+            }
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users,email,'.$user->id,
+                'password' => 'required|string|min:8',
+            ]);
+            $user->update($validatedData);
+            DB::commit();
+            return response()->json($user, 200);
+        } catch (\Exception $exception){
+            DB::rollBack();
+            return response()->json(['error' => $exception->getMessage()], 500);
         }
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users,email,'.$user->id,
-            'password' => 'required|string|min:8',
-        ]);
-
-        $user->update($validatedData);
-        return response()->json($user, 200);
     }
 
     /**
