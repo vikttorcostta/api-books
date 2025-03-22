23fb0aacd0dfe9d8d5dce9842270a03e1aacc657
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-22 19:57:26 -0300

    criação do CategoryController

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Category::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validationData = $request->validate([
+                'name' => 'required|max:255',
+            ]);
+
+            $category = Category::create($validationData);
+            DB::commit();
+            return response()->json($category, 201);
+
+        }catch (\Exception $exception){
+            DB::rollBack();
+            return response()->json($exception->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::find($id);
+        if (is_null($category)) {
+            return response()->json(["message" => "Categoria não encontrada"], 404);
+        }
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        try {
+            $category = Category::find($id);
+
+            if (is_null($category)) {
+                return response()->json(["message" => "Categoria não encontrada"], 404);
+            }
+
+            $validatedData = $request->validate([
+                'name' => 'required|max:255',
+            ]);
+
+            $category->update($validatedData);
+            DB::commit();
+            return response()->json($category, 200);
+
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return response()->json($exception->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+        if (is_null($category)) {
+            return response()->json(["Message" => "Categoria não encontrada"], 404);
+        }
+        $category->delete();
+        return response()->json(["Message" => "Categoria excluída com sucesso"], 201);
+    }
+}
