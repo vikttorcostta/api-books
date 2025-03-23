21dea44a10b4b66b1a00f06b52f4405b3dea776b
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-22 22:25:27 -0300

    rota de books

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,4 +11,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::apiResource('users', UserController::class);
-
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('books', BookController::class);
