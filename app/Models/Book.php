cfac60c0275c7cd21ae32316310da8e63097973b
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-22 22:27:31 -0300

    criação de book model

---
+++ app/Models/Book.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\StatusBook;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'cover', 'author', 'publisher', 'publication_year', 'isbn', 'status'];
+
+    protected $casts = [
+        'status' => StatusBook::class,
+    ];
+    protected $appends = ['cover_url'];
+    public function getCoverUrlAttribute(){
+        return $this->cover ? asset('storage/' . $this->cover) : null;
+    }
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+
+}
