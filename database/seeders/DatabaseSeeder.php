ca2a8dcba2ecf1da5cb2e1f82a61e3112ab26397
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-24 09:25:23 -0300

    ajuste seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Review;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,157 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Paulo Victor',
-            'email' => 'paulovictor@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Roque Leto',
-            'email' => 'roqueleto@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Davi Caridade',
-            'email' => 'davi@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Vandson Nascimento',
-            'email' => 'vandson@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Gabriel Gomes',
-            'email' => 'gabrielgomes@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Alice Moraes',
-            'email' => 'alicemoraes@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Bruno Ribeiro',
-            'email' => 'brunoribeiro@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Carla Souza',
-            'email' => 'carlasouza@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Daniel Fernandes',
-            'email' => 'danielfernandes@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Eduarda Lima',
-            'email' => 'eduardalima@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Felipe Castro',
-            'email' => 'felipecastro@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Gabriela Nunes',
-            'email' => 'gabrielanunes@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Henrique Martins',
-            'email' => 'henriquemartins@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Isabela Rocha',
-            'email' => 'isabelarocha@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'João Silva',
-            'email' => 'joaosilva@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Karen Oliveira',
-            'email' => 'karenoliveira@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Leonardo Santos',
-            'email' => 'leonardosantos@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Mariana Teixeira',
-            'email' => 'marianateixeira@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Nathan Costa',
-            'email' => 'nathancosta@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Olivia Alves',
-            'email' => 'oliviaalves@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Paulo Mendes',
-            'email' => 'paulomendes@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Raquel Farias',
-            'email' => 'raquelfarias@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Samuel Pereira',
-            'email' => 'samuelpereira@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Tatiane Borges',
-            'email' => 'tatianeborges@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Vinícius Gomes',
-            'email' => 'viniciusgomes@books.com.br',
-            'password' => bcrypt('12345678'),
-        ]);
+//        User::factory(30)->create();
+//        Category::factory(30)->create();
+//        Book::factory(30)->create();
+//        Review::factory(30)->create();
 
     }
 }
